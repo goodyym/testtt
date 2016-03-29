a54a302730e154a4e76b67fa2102fb02379f4ab5
yym123 <ym.yuan@haitongwangxiao.com>
2016-03-29 16:33:47 +0800

    Merge branch 'master' of github.com:goodyym/testtt
    # Conflicts:
    #	src/main/java/com/ggg/HomePage.java

--- src/main/java/com/ggg/HomePage.java
+++ src/main/java/com/ggg/HomePage.java
@@ -11,7 +11,7 @@ public class HomePage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 	private int a = 5;
-	private String s = "ttt";
+	private String s = "tttbbbbbbbbbbbbbbbbbbb";
 	private String SS = "kk";
 
 	// TODO Add any page properties or variables here
