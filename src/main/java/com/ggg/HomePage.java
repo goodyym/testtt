6a22823e55b021300f00b3464756c9438215e958
yym123 <ym.yuan@haitongwangxiao.com>
2016-03-29 16:20:31 +0800

    Merge branch 'master' of github.com:goodyym/testtt
    # Conflicts:
    #	src/main/java/com/ggg/HomePage.java

--- src/main/java/com/ggg/HomePage.java
+++ src/main/java/com/ggg/HomePage.java
@@ -24,7 +24,6 @@ public class HomePage extends WebPage {
 
 
         System.out.println("777766666666667");
-        System.out.println("777yyyyyyy77");
         System.out.println("99999999995559999");
     }
 }
